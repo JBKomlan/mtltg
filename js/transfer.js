c2af5c2d11b264060168438b83df64379e5bdedf
JBKomlan <147122308+JBKomlan@users.noreply.github.com>
2026-04-17 13:37:00 +0000

    Fix input event listener to replace commas and periods

--- js/transfer.js
+++ js/transfer.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const mtField = document.getElementById("mtField");
   if (mtField) {
     mtField.addEventListener("input", () => {
-  mtField.value = mtField.value.replace(/[.,]/g, "");
+    mtField.value = mtField.value.replace(/[.,]/g, "");
   drawButtons(p);
   });
      
