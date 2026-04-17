277839ce78545cc660d89900a2603444dc495c1e
JBKomlan <147122308+JBKomlan@users.noreply.github.com>
2026-04-17 12:36:40 +0000

    Fix input field formatting in montantBlock

--- js/transfer.js
+++ js/transfer.js
@@ -62,7 +62,7 @@ function buildUI(p) {
   const montantBlock = p.e === "0"
     ? `<div class="amount-display">${formatMontant(p.mt)}</div>`
     : `<div class="input-fcfa">
-         <input type="number" id="mtField" class="amount-input" value="${p.mt}" min="${p.mt}" autofocus>
+         <input autofocus type="number" id="mtField" class="amount-input" value="${p.mt}" min="${p.mt}">
          <span>FCFA</span>
        </div>`;
 
